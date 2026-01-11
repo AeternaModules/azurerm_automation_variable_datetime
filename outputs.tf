09092ebcc9d3839cfac4d668cc7fd12d6cb8afe3
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:56:38 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "automation_variable_datetimes" {
+  description = "All automation_variable_datetime resources"
+  value       = azurerm_automation_variable_datetime.automation_variable_datetimes
+}
+
