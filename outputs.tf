2f7fe352d8e1e7fcec2f13d945bd2304e5c6f9ec
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "automation_variable_datetimes" {
-  description = "All automation_variable_datetime resources"
-  value       = azurerm_automation_variable_datetime.automation_variable_datetimes
-}
 output "automation_variable_datetimes_automation_account_name" {
-  description = "List of automation_account_name values across all automation_variable_datetimes"
-  value       = [for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_variable_datetimes, keyed the same as var.automation_variable_datetimes"
+  value       = { for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : k => v.automation_account_name }
 }
 output "automation_variable_datetimes_description" {
-  description = "List of description values across all automation_variable_datetimes"
-  value       = [for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : v.description]
+  description = "Map of description values across all automation_variable_datetimes, keyed the same as var.automation_variable_datetimes"
+  value       = { for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : k => v.description }
 }
 output "automation_variable_datetimes_encrypted" {
-  description = "List of encrypted values across all automation_variable_datetimes"
-  value       = [for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : v.encrypted]
+  description = "Map of encrypted values across all automation_variable_datetimes, keyed the same as var.automation_variable_datetimes"
+  value       = { for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : k => v.encrypted }
 }
 output "automation_variable_datetimes_name" {
-  description = "List of name values across all automation_variable_datetimes"
-  value       = [for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : v.name]
+  description = "Map of name values across all automation_variable_datetimes, keyed the same as var.automation_variable_datetimes"
+  value       = { for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : k => v.name }
 }
 output "automation_variable_datetimes_resource_group_name" {
-  description = "List of resource_group_name values across all automation_variable_datetimes"
-  value       = [for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_variable_datetimes, keyed the same as var.automation_variable_datetimes"
+  value       = { for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : k => v.resource_group_name }
 }
 output "automation_variable_datetimes_value" {
-  description = "List of value values across all automation_variable_datetimes"
-  value       = [for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : v.value]
+  description = "Map of value values across all automation_variable_datetimes, keyed the same as var.automation_variable_datetimes"
+  value       = { for k, v in azurerm_automation_variable_datetime.automation_variable_datetimes : k => v.value }
 }
 
